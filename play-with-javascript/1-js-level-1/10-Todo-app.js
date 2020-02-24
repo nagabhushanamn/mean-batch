c20891b23ee4a84da362c4783fa600b36f765d93
nagabhushanam N <nagabhushanamn@gmail.com>
2020-02-24 15:47:00 +0530

    Todo App

--- play-with-javascript/1-js-level-1/10-Todo-app.js
+++ play-with-javascript/1-js-level-1/10-Todo-app.js
@@ -1,21 +1,31 @@
 
 
-// Model / data
+
 /**
  * 
  *  Todo =>  {id,title,completed}
  * 
  */
 
+// Model / data
 class Todo {
     constructor(title) {
-        //....
+        Todo.idx++
+        this.id = Todo.idx
+        this.title = title
+        this.completed = false
     }
 }
+Todo.idx = 0;
 
+// service
 class TodoService {
+    constructor() {
+        this.todos = []
+    }
     addTodo(title) {
-        //..
+        let newTodo = new Todo(title)
+        this.todos = this.todos.concat(newTodo)
     }
     editTodo(idx, newTitle) {
         //..
@@ -34,9 +44,13 @@ class TodoService {
     }
     viewTodos(flag) {
         //..
+        if (flag === "ALL")
+            this.todos.forEach(todo => console.log(todo))
     }
 }
 
 
 let service = new TodoService();
-
+service.addTodo("go to BLT")
+service.addTodo("go to ROOM")
+service.viewTodos('ALL')
